4204e52c3b8ea919f92eecd6674d37d73ae0fe23
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-08 11:01:12 -0700

    Will test to see that user's favorite color is correctly stored.

--- __tests__/space-ager.test.js
+++ __tests__/space-ager.test.js
@@ -10,7 +10,7 @@ describe("SpaceAger", () => {
   })
 
   test("Should create a new class object that will store the age for Mercury,Venus,Mars and Jupiter", () => {
-    const userAge = 46;
+    const userAge = 25;
 
     const ageSpace = new SpaceAger(userAge);
     expect(ageSpace.earthAge).toEqual(userAge);
@@ -18,5 +18,7 @@ describe("SpaceAger", () => {
     expect(ageSpace.venusAge).toEqual(userAge/0.62);
     expect(ageSpace.marsAge).toEqual(userAge/1.88);
     expect(ageSpace.jupiterAge).toEqual(userAge/11.86);
+    console.log(ageSpace.jupiterAge);
+    console.log(ageSpace.mercuryAge);
   })
 })
