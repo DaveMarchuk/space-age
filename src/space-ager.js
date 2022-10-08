dbb10da0fdf67300ba9a00dcbf20d0370149f062
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-08 10:48:20 -0700

    Passed test where ages for other planets are stored.

--- src/space-ager.js
+++ src/space-ager.js
@@ -1,6 +1,10 @@
 export default class SpaceAger  {
   constructor(userAge)  {
     this.earthAge = userAge;
+    this.mercuryAge = userAge/0.24;
+    this.venusAge = userAge/0.62;
+    this.marsAge = userAge/1.88;
+    this.jupiterAge = userAge/11.86;
   }
 
 }
