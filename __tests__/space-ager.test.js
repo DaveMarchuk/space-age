79e49616999ec752b2b0e99e4e8bf16b38541d5b
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-08 11:03:51 -0700

    Succesfully passed test to see if color is being stored.

--- __tests__/space-ager.test.js
+++ __tests__/space-ager.test.js
@@ -5,14 +5,14 @@ describe("SpaceAger", () => {
   test("Should create a new class object that will store the users Earth age", () => {
     const userAge = 27;
 
-    const ageSpace = new SpaceAger(userAge);
+    let ageSpace = new SpaceAger(userAge);
     expect(ageSpace.earthAge).toEqual(userAge);
   })
 
   test("Should create a new class object that will store the age for Mercury,Venus,Mars and Jupiter", () => {
     const userAge = 25;
 
-    const ageSpace = new SpaceAger(userAge);
+    let ageSpace = new SpaceAger(userAge);
     expect(ageSpace.earthAge).toEqual(userAge);
     expect(ageSpace.mercuryAge).toEqual(userAge/0.24);
     expect(ageSpace.venusAge).toEqual(userAge/0.62);
@@ -21,4 +21,11 @@ describe("SpaceAger", () => {
     console.log(ageSpace.jupiterAge);
     console.log(ageSpace.mercuryAge);
   })
+
+  test("Will test if user is able to store their favorite color",() =>  {
+    const favColor = "blue";
+    
+    let ageSpace = new SpaceAger(25,favColor);
+    expect(ageSpace.favColor).toEqual(favColor);
+  }) 
 })
--- src/space-ager.js
+++ src/space-ager.js
@@ -1,10 +1,11 @@
 export default class SpaceAger  {
-  constructor(userAge)  {
+  constructor(userAge,userColor)  {
     this.earthAge = userAge;
     this.mercuryAge = userAge/0.24;
     this.venusAge = userAge/0.62;
     this.marsAge = userAge/1.88;
     this.jupiterAge = userAge/11.86;
+    this.favColor = userColor;
   }
 
 }
